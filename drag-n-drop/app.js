792647fd230c5f00a33aba3070622a4f66984466
GSergey <example@gmail.com>
2021-09-16 00:46:10 +0300

    init: drag-n-drop

---
+++ drag-n-drop/app.js
@@ -0,0 +1,44 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+}
+
+function dragstart(event) {
+    // console.log('drag start', event.target);
+    event.target.classList.add('hold');
+    setTimeout(() => event.target.classList.add('hide'), 0);
+
+}
+
+function dragend(event) {
+    // console.log('drag end', event.target);
+    // event.target.classList.remove('hold', 'hide');
+    event.target.className = 'item'; //устанавливает для event класс item (удаляет hold и hide)
+
+}
+
+function dragover(event) {
+    event.preventDefault();
+    console.log('drag over');
+}
+function dragenter(event) {
+    event.target.classList.add('hovered');
+    console.log('drag enter');
+}
+function dragleave(event) {
+    event.target.classList.remove('hovered');
+    console.log('drag leave');
+}
+function dragdrop(event) {
+    event.target.classList.remove('hovered');
+    event.target.append(item);
+    console.log('drop');
+}
